db1ee7c8d61dd0652a5a23108025e49a7412146e
nguyen-huuhien <huuhien3099@gmail.com>
2020-01-03 16:02:55 +0700

    Recommit

---
+++ c1019i1_nguyenhuuhien/Module_2/src/b6_CauTrucDuLieuVaGiaiThuatCoBan/BT/TrienKhaiCacPhuongThucCuaArrayListTheoThuVienJaVa_ulti_arrlist/MyList.java
@@ -0,0 +1,27 @@
+package b6_CauTrucDuLieuVaGiaiThuatCoBan.BT.TrienKhaiCacPhuongThucCuaArrayListTheoThuVienJaVa_ulti_arrlist;
+
+import javax.xml.soap.Node;
+
+public class MyList<E> {
+
+    int size = 0;
+    static final int DEFAULT_CAPACITY = 10;
+    Object elements[];
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void Mylist(int capacity) {
+        elements = new Object[capacity];
+    }
+
+    public void add(int index, E element) {
+
+        for
+
+    }
+
+
+
+}

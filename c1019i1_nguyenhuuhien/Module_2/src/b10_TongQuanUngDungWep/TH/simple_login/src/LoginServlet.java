f0849c24c860afb08937b3de6fcd246770ddfcb4
nguyen-huuhien <huuhien3099@gmail.com>
2020-02-07 16:46:36 +0700

    recommit

--- c1019i1_nguyenhuuhien/Module_2/src/b10_TongQuanUngDungWep/TH/simple_login/src/LoginServlet.java
+++ c1019i1_nguyenhuuhien/Module_2/src/b10_TongQuanUngDungWep/TH/simple_login/src/LoginServlet.java
@@ -4,14 +4,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "LoginServlet")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
 
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        if("admin".equals(username) && "admin".equals(password)) {
+            writer.println("<h1>Welcome "+username+" to website</h1>");
+        } else{
+            writer.println("<h1>Login Error</h1>");
+        }
+
+        writer.println("</html>");
     }
 }

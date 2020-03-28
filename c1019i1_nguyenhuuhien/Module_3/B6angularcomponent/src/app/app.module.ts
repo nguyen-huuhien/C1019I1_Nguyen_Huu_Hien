5a05ace0b76454fab352c28e90edecc95296e3e3
nguyen-huuhien <huuhien3099@gmail.com>
2020-03-28 15:58:54 +0700

    28/3

--- c1019i1_nguyenhuuhien/Module_3/B6angularcomponent/src/app/app.module.ts
+++ c1019i1_nguyenhuuhien/Module_3/B6angularcomponent/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
+    CountdownTimerComponent
   ],
   imports: [
     BrowserModule
